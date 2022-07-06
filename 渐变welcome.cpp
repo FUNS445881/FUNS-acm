8a155c364b063e54d9baf25f33c917801be32c79
FUNS445881 <108578632+FUNS445881@users.noreply.github.com>
2022-07-06 20:39:03 +0800

    Add files via upload

---
+++ 渐变welcome.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+int main()
+{
+	char arr1[] = "Welcome to My World!";
+	char arr2[] = "####################";
+	int left = 0, right = 0, st = 0;
+	st = strlen(arr1) - 1;
+	right = st;
+	while (left <= right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		left++, right--;
+		Sleep(1000);
+		printf("%s\n", arr2);
+	};
+	return 0;
+}
