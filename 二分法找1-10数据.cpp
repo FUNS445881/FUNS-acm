f0b06c8c8b068ddf4f7fcaed2a88f9a84274a9a3
FUNS445881 <108578632+FUNS445881@users.noreply.github.com>
2022-07-06 18:45:47 +0800

    Add files via upload

---
+++ 二分法找1-10数据.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sk,right,mid=0,k = 0;
+	int left = 0;
+	scanf("%d", &k);
+	sk = sizeof(arr) / sizeof(arr[0]);
+	right = sk - 1;
+	while (left <= right)
+	{
+	     mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else 
+		{
+			printf("找到了，下标是:%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("找不到");
+	}
+	return 0;
+}
