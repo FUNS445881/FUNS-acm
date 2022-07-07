4e173d3216fd7983b612812d597cbcda2d626dd8
FUNS445881 <108578632+FUNS445881@users.noreply.github.com>
2022-07-07 10:35:50 +0800

    Update 最大公约数.cpp

--- 最大公约数.cpp
+++ 最大公约数.cpp
@@ -5,14 +5,15 @@ int main()
 	int m, n, r;
 	int p = 0;
 	scanf("%d%d", &m, &n);
-	if (m < n)
-	{
-		p = m;
-		m = n;
-		n = p;
-	}
-		r = m % n;
-	while (r = m%n)
+	//经过测试，无需比较大小，原理为若m<n,则在第一次循环中进行r=m，m=n后mn的关系为m>n.
+	//if (m < n)
+	//{
+	//	p = m;
+	//	m = n;
+	//	n = p;
+	//}
+	r = m % n;
+	while (r = m % n)
 	{
 		m = n;
 		n = r;
