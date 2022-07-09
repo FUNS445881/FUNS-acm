83c5556361df3c0abaf7711ec930c1c6eeb77564
FUNS445881 <108578632+FUNS445881@users.noreply.github.com>
2022-07-09 20:50:56 +0800

    Add files via upload

---
+++ 99乘法表.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int j = 0;
+	for (i = 1; i <= 9; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%-2d ", i, j, i * j);
+		}
+		printf("\n");
+	}
+	return 0;
+}
