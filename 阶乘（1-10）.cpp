aa8d789dd535c52ed4edbd7c7ccff8d59984b661
FUNS445881 <108578632+FUNS445881@users.noreply.github.com>
2022-08-16 15:35:44 +0800

    Update 阶乘（1-10）.cpp

--- 阶乘（1-10）.cpp
+++ 阶乘（1-10）.cpp
@@ -10,5 +10,13 @@ int main()
 		sum = sum + a;
 	}
 	printf("%d\n", sum);
+		//方法2 函数法
+	//void fac2(int n)
+	//{
+	//    if(n<=1)
+	//      return 1;
+	//    else
+	//      return n *fac2(n-1);
+	//}
 	return 0;
 }
