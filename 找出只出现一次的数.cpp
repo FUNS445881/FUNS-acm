a9288e7c2d47b2f2019546dec282d5104f483c18
FUNS445881 <108578632+FUNS445881@users.noreply.github.com>
2022-08-16 15:59:52 +0800

    Add files via upload

---
+++ 找出只出现一次的数.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	int arr[] = { 4,4,5,8,8,1,1};
+	int i = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//方法1-暴力求解
+	for (i = 0; i < sz; i++)
+	{
+		int count = 0;//计数器，用来计数出现了几次
+		int j = 0;//important：j每一次都要初始化为0才可以
+		for (j = 0; j < sz; j++)
+		{
+			if (arr[i] == arr[j])
+			{
+				count++;
+			}		
+		}
+		if (count == 1)
+		{
+		printf("只出现一次的数为：%d", arr[i]);
+			break;
+		}
+	}
+	return 0;
+}
